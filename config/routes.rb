cd8f0f3e6656637f0c0c8c11bee4f813f05e66a3
tech-camp <tech-camp@A-267.local>
2020-08-18 17:45:00 +0900

    messageの送信機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
   resources :users
-  resources :groups
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
